b8e064fab90b8f477614be5f5a367151a61c31fc
Tiago Cunha <tcunha_lp@hotmail.com>
2019-03-16 22:20:10 +0000

    gh-pages

--- src/about/aboutmain/portfolio/portfoliomain/portfoliomain.js
+++ src/about/aboutmain/portfolio/portfoliomain/portfoliomain.js
@@ -6,7 +6,7 @@ import Project from './project';
 const Project1 = {
     title: 'Real Estate Website',
     tags: 'wordpress',
-    image: '/images/project1.png',
+    image: '/public/images/project1.png',
     description: 'An Openclassrooms course project',
     plink: 'http://dev-chaletsandcav.pantheonsite.io/'
 }
@@ -14,13 +14,13 @@ const Project1 = {
 const Project2 = {
     title: 'Film Festival',
     tags: 'html, css, bootstrap', 
-    image: '/images/project2.png',
+    image: '/public/images/project2.png',
     description: 'An Openclassrooms course project'
 }
 const Project3 = {
     title: 'Portfolio',
     tags: 'react, react-router, react-bootstrap, css', 
-    image: '/images/project3.png',
+    image: '/public/images/project3.png',
     description: 'This Website! Also an Openclassrooms course project'
 }
 
