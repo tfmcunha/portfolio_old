1c06b23aa19fe429c042bb0cb180378748f63947
Tiago Cunha <tcunha_lp@hotmail.com>
2019-03-16 23:08:46 +0000

    Updates

---
+++ precache-manifest.2c3791897fa764c35d0166013d311f0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "2c1f9e30a6a6b8f0bea1",
+    "url": "/portfolio/static/js/main.2c1f9e30.chunk.js"
+  },
+  {
+    "revision": "eae6805ca9078abbf5bf",
+    "url": "/portfolio/static/js/1.eae6805c.chunk.js"
+  },
+  {
+    "revision": "2c1f9e30a6a6b8f0bea1",
+    "url": "/portfolio/static/css/main.60bc3477.chunk.css"
+  },
+  {
+    "revision": "4997c6afd1222cf1328013a87d6bc965",
+    "url": "/portfolio/index.html"
+  }
+];
